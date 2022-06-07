37610332068d31e3f0cef6e984b179b1e45f8c39
Anthony Reyes <reyesanthony8@gmail.com>
2022-06-06 20:24:26 -0400

    error message

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -7,19 +7,18 @@ const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
-console.log("1")
 const reservationsRouter = require("./reservations/reservations.router");
 const tablesRouter = require("./tables/tables.router");
-console.log("2")
+
 const app = express();
-console.log("3")
+
 app.use(cors());
 app.use(express.json());
-console.log(4)
+
 app.use("/reservations", reservationsRouter);
 app.use("/tables", tablesRouter);
-console.log(5)
+
 app.use(notFound);
 app.use(errorHandler);
-console.log(6)
+
 module.exports = app;
